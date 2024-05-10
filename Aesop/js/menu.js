0220034fe66b08a98dced70f2c824b33f8517d16
Moontari <shaaa6256@gmail.com>
2024-05-10 14:56:34 +0900

    이솝 홈페이지 리뉴얼 프로젝트 모바일 반응형

--- Aesop/js/menu.js
+++ Aesop/js/menu.js
@@ -1,11 +1,39 @@
-$(function () {
-  var $header_list = $('.header > .header_wrap > .header_cont .header_list');
-  var $header = $('.header');
-  $header_list
-    .mouseenter(function () {
-      $header.stop().animate({ height: '500px' }, 30);
-    })
-    .mouseleave(function () {
-      $header.stop().animate({ height: '60px' }, 30);
-    });
+// 헤더
+$(document).ready(function () {
+  $(".nav .depth_1 > li, .menu_bg").hover(
+    function () {
+      $(".header").addClass("active");
+    },
+    function () {
+      $(".header").removeClass("active");
+    }
+  );
+  //GNB / NAV
+  $(".nav .depth_2 > li").hover(
+    function () {
+      $(".nav .depth_1 > li").removeClass("active");
+
+      $(this).parents("li").addClass("active");
+    },
+    function () {
+      $(".nav .depth_1 > li").removeClass("active");
+      $(this).parents("li").removeClass("active");
+    }
+  );
+  // 모바일
+  $(".btn").click(function () {
+    $(".mob_menu,.page_cover,html").addClass("open");
+    window.location.hash = "#open";
+  });
+
+  window.onhashchange = function () {
+    if (location.hash != "#open") {
+      $(".mob_menu,.page_cover,html").removeClass("open");
+    }
+  };
+
+  $(".mob_menu ul.sub_mobile").hide();
+  $(".mob_menu ul.nav li").click(function () {
+    $("ul", this).slideToggle("fast");
+  });
 });
